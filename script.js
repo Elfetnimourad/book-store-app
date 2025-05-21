67114398fa707c7d4a21d20ba07390f67c782e98
Elfetnimourad <moradtao2000@gmail.com>
2025-05-21 23:11:57 +0100

    some ui and routing and etc...

--- script.js
+++ script.js
@@ -4,19 +4,24 @@ let upPart = document.querySelector(".up-part");
 menuIcon.addEventListener("click", function () {
   let sideBar = document.createElement("div");
   //   let [Home, About, Category, ContactUs] = document.createElement("h6");
-  let Home = document.createElement("h6");
-  let About = document.createElement("h6");
-  let Category = document.createElement("h6");
-  let ContactUs = document.createElement("h6");
+  let Home = document.createElement("a");
+  let About = document.createElement("a");
+  let Category = document.createElement("a");
+  let ContactUs = document.createElement("a");
   let closeIcon = document.createElement("i");
-  closeIcon.className = "fa fa-times";
+  closeIcon.className = "bi bi-arrow-left";
   closeIcon.style.color = "white";
-  closeIcon.style.height = "200px";
+  closeIcon.style.lineHeight = "50px";
+  menuIcon.style.display = "none";
+  //   closeIcon.style.backgroundColor = "red";
+  About.href = "/About.html";
+  Home.href = "/";
+  ContactUs.href = "/ContactUs.html";
 
-  Home.innerHTML = "Home";
-  About.innerHTML = "About";
-  Category.innerHTML = "Category";
-  ContactUs.innerHTML = "Contact Us";
+  Home.innerText = "Home";
+  About.innerText = "About";
+  Category.innerText = "Category";
+  ContactUs.innerText = "Contact Us";
   sideBar.style.display = "flex";
   sideBar.style.flexDirection = "column";
   sideBar.style.justifyContent = "flex-start";
@@ -26,13 +31,46 @@ menuIcon.addEventListener("click", function () {
   sideBar.appendChild(Category);
   sideBar.appendChild(ContactUs);
 
-  sideBar.style.width = "250px";
+  sideBar.style.width = "30%";
   sideBar.style.height = "100vh";
   sideBar.style.position = "fixed";
   sideBar.style.zIndex = "9999";
   sideBar.style.backgroundColor = "black";
   sideBar.style.opacity = "0.9";
   upPart.appendChild(sideBar);
+
+  // route
+  // const route = (event) => {
+  //   event = event || window.event;
+  //   event.preventDefault();
+  //   window.history.pushState({}, "", event.target.href);
+  //   handleLocation();
+  // };
+  // const routes = {
+  //   404: "/404.html",
+  //   "/": "/index.html",
+  //   "/About": "/About.html",
+  //   "/ContactUs": "/ContactUs.html",
+  // };
+  const handleLocation = async () => {
+    // const path = window.location.pathname;
+    // const route = routes[path] || routes[404];
+    // await fetch(route).then((data) => data.text());
+    // document.getElementById("main-page").innerHTML = html;
+  };
+
+  // window.onpopstate = handleLocation;
+  // window.route = route;
+
+  // handleLocation();
+
+  // About.onclick = route();
+
+  //close Icon
+  closeIcon.addEventListener("click", function () {
+    sideBar.remove();
+    menuIcon.style.display = "flex";
+  });
 });
 async function getData() {
   try {
@@ -46,14 +84,15 @@ async function getData() {
     arr.map((element) => {
       let cardBook = document.createElement("div");
       let bookCover = document.createElement("img");
-      let bookTitle = document.createElement("h6");
-      cardBook.style.width = "318px";
-      cardBook.style.height = "318px";
+      let bookTitle = document.createElement("p");
+      //   cardBook.style.width = "24%";
+      cardBook.style.height = "100%";
       cardBook.style.borderRadius = "10px";
       cardBook.style.backgroundColor = "white";
+      cardBook.className = "card-book";
       bookCover.src = `https://covers.openlibrary.org/b/id/${element.cover_id}-M.jpg`;
-      bookCover.style.width = "95%";
-      bookCover.style.height = "95%";
+      bookCover.style.width = "99%";
+      bookCover.style.height = "80%";
 
       bookTitle.innerText = element.title;
       cardBook.appendChild(bookCover);
@@ -64,5 +103,8 @@ async function getData() {
     console.log(err);
   }
 }
+window.onscroll = function () {
+  upPart.classList.add("fixed");
+};
 getData();
 //63a6b97b
