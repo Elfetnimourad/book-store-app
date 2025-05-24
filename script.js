63e419573dc58b099d5594b3b8f53dde07bf97de
Elfetnimourad <moradtao2000@gmail.com>
2025-05-24 23:29:02 +0100

    formatting and logic

--- script.js
+++ script.js
@@ -2,135 +2,32 @@ let bookPart = document.querySelector(".book-part");
 let menuIcon = document.querySelector("i");
 let upPart = document.querySelector(".up-part");
 let input = document.querySelector("input");
+let sideBar = document.querySelector(".sidebar");
+let closeIcon = document.querySelector("#closeIcon");
+let allOptionsOfSelect = document.querySelectorAll("option");
 
+console.log(allOptionsOfSelect);
 let url = "https://openlibrary.org/subjects/history.json?limit=20";
 let array = [];
-menuIcon.addEventListener("click", function () {
-  let sideBar = document.createElement("div");
-  //   let [Home, About, Category, ContactUs] = document.createElement("h6");
-  let Home = document.createElement("a");
-  let About = document.createElement("a");
-  let Category = document.createElement("select");
-  let ContactUs = document.createElement("a");
-  let closeIcon = document.createElement("i");
-  let optGr = document.createElement("optgroup");
-  closeIcon.className = "bi bi-arrow-left";
-  closeIcon.style.color = "white";
-  closeIcon.style.lineHeight = "50px";
-  menuIcon.style.display = "none";
-  //   closeIcon.style.backgroundColor = "red";
-  About.href = "/pages/About.html";
-  Home.href = "/";
-  ContactUs.href = "/pages/ContactUs.html";
-
-  Home.innerText = "Home";
-  About.innerText = "About";
-  Category.innerText = "Category";
-  ContactUs.innerText = "Contact Us";
-  sideBar.style.display = "flex";
-  sideBar.style.flexDirection = "column";
-  sideBar.style.justifyContent = "flex-start";
-  sideBar.appendChild(closeIcon);
-  sideBar.appendChild(Home);
-  sideBar.appendChild(About);
-  sideBar.appendChild(Category);
-  sideBar.appendChild(ContactUs);
-
-  sideBar.style.width = "30%";
-  sideBar.style.height = "100vh";
-  sideBar.style.position = "fixed";
-  sideBar.style.zIndex = "9999";
-  sideBar.style.backgroundColor = "black";
-  sideBar.style.opacity = "0.9";
-  upPart.appendChild(sideBar);
-  //side bar for category
-
-  // Category.appendChild(categoryLabel);
-  let opt1 = document.createElement("option");
-  let opt2 = document.createElement("option");
-  let opt3 = document.createElement("option");
-  let opt4 = document.createElement("option");
-  opt1.innerText = "Category";
-  opt2.innerText = "programming";
-  opt3.innerText = "history";
-  opt4.innerText = "math";
-  if (opt2.value === "programming") {
-  }
-  opt2.addEventListener(
-    "click",
-    getData(`https://openlibrary.org/subjects/programming.json?limit=20`)
-  );
-
-  // arr.addEventListener("click", function () {
-  //   subject = opt2.value;
-  //   console.log(subject);
-  // });
-  optGr.appendChild(opt1);
-  optGr.appendChild(opt2);
-  optGr.appendChild(opt3);
-  optGr.appendChild(opt4);
-  Category.appendChild(optGr);
-  console.log(optGr.children.item(1).textContent, "this opt");
-  Category.addEventListener("click", function () {
-    // console.log("from contact us");
-    // let sideBarForCategory = document.createElement("div");
-    // sideBarForCategory.style.height = "20vh";
-    // sideBarForCategory.style.width = 10%";
-    // sideBarForCategory.style.backgroundColor = "white";
-    // sideBarForCategory.style.opacity = "0.9";
-    // sideBarForCategory.style.zIndex = "99999";
-    // // Category.style.display = "flex";
-    // sideBar.appendChild(sideBarForCategory);
-  });
-
-  // route
-  const route = (event) => {
-    event = event || window.event;
-    event.preventDefault();
-    window.history.pushState({}, "", event.target.href);
-    handleLocation();
-  };
-  const routes = {
-    404: "/404.html",
-    "/": "/index.html",
-    "/About": "/pages/About.html",
-    "/ContactUs": "/pages/ContactUs.html",
-  };
-  const handleLocation = async () => {
-    const path = window.location.pathname;
-    const route = routes[path] || routes[404];
-    await fetch(route).then((data) => data.text());
-    // document.getElementById("main-page").innerHTML = html;
-  };
-
-  window.onpopstate = handleLocation;
-  window.route = route;
-
-  handleLocation();
-
-  About.onclick = route();
-
-  //close Icon
-  closeIcon.addEventListener("click", function () {
-    sideBar.remove();
-    menuIcon.style.display = "flex";
-  });
+closeIcon.addEventListener("click", function () {
+  sideBar.classList.toggle("d-none", true);
 });
-async function getData() {
+menuIcon.addEventListener("click", function () {
+  sideBar.classList.toggle("d-none", false);
+});
+async function getData(url) {
   try {
-    const res = await fetch(
-      `https://openlibrary.org/subjects/programming.json?limit=20`
-    );
+    const res = await fetch(url);
 
     const data = await res.json();
     let arr = data.works;
     console.log(data);
     console.log(arr);
     input.onchange = function (e) {
-      arr = arr?.filter((ele) => ele.title === e.target.value);
+      let arrFilter = arr?.filter((ele) => ele.title === e.target.value);
       console.log(arr);
     };
-    arr = arr?.forEach((element) => {
+    arr?.forEach((element) => {
       let cardBook = document.createElement("div");
       let bookCover = document.createElement("img");
       let bookTitle = document.createElement("p");
@@ -142,10 +39,14 @@ async function getData() {
       bookCover.src = `https://covers.openlibrary.org/b/id/${element.cover_id}-M.jpg`;
       bookCover.style.width = "99%";
       bookCover.style.height = "80%";
-
+      let bookPdf = document.createElement("a");
+      bookPdf.href = `https://openlibrary.org/${element.key}/${element.title}.pdf`;
+      bookPdf.innerHTML = "read";
       bookTitle.innerText = element.title;
       cardBook.appendChild(bookCover);
       cardBook.appendChild(bookTitle);
+      cardBook.appendChild(bookPdf);
+
       bookPart?.appendChild(cardBook);
     });
   } catch (err) {
@@ -155,5 +56,131 @@ async function getData() {
 window.onscroll = function () {
   upPart.classList.add("fixed");
 };
-getData();
+// getData(`https://openlibrary.org/subjects/science.json?limit=20`);
 //63a6b97b
+
+// function () {
+//   let sideBar = document.createElement("div");
+//   //   let [Home, About, Category, ContactUs] = document.createElement("h6");
+//   let Home = document.createElement("a");
+//   let About = document.createElement("a");
+//   let Category = document.createElement("select");
+//   let ContactUs = document.createElement("a");
+//   let closeIcon = document.createElement("i");
+//   let optGr = document.createElement("optgroup");
+//   closeIcon.className = "bi bi-arrow-left";
+//   closeIcon.style.color = "white";
+//   closeIcon.style.lineHeight = "50px";
+//   menuIcon.style.display = "none";
+//   //   closeIcon.style.backgroundColor = "red";
+//   About.href = "/pages/About.html";
+//   Home.href = "/";
+//   ContactUs.href = "/pages/ContactUs.html";
+
+//   Home.innerText = "Home";
+//   About.innerText = "About";
+//   Category.innerText = "Category";
+//   ContactUs.innerText = "Contact Us";
+//   sideBar.style.display = "flex";
+//   sideBar.style.flexDirection = "column";
+//   sideBar.style.justifyContent = "flex-start";
+//   sideBar.appendChild(closeIcon);
+//   sideBar.appendChild(Home);
+//   sideBar.appendChild(About);
+//   sideBar.appendChild(Category);
+//   sideBar.appendChild(ContactUs);
+
+//   sideBar.style.width = "30%";
+//   sideBar.style.height = "100vh";
+//   sideBar.style.position = "fixed";
+//   sideBar.style.zIndex = "9999";
+//   sideBar.style.backgroundColor = "black";
+//   sideBar.style.opacity = "0.9";
+//   upPart.appendChild(sideBar);
+//   //side bar for category
+
+//   // Category.appendChild(categoryLabel);
+//   let opt1 = document.createElement("option");
+//   let opt2 = document.createElement("option");
+//   let opt3 = document.createElement("option");
+//   let opt4 = document.createElement("option");
+//   opt1.innerText = "science";
+//   opt2.innerText = "programming";
+//   opt3.innerText = "history";
+//   opt4.innerText = "math";
+
+//   if (opt2.value === "programming") {
+//   }
+
+//   // opt2.addEventListener(
+//   //   "click",
+//   // );
+
+//   // arr.addEventListener("click", function () {
+//   //   subject = opt2.value;
+//   //   console.log(subject);
+//   // });
+//   optGr.appendChild(opt1);
+//   optGr.appendChild(opt2);
+//   optGr.appendChild(opt3);
+//   optGr.appendChild(opt4);
+//   Category.appendChild(optGr);
+//   console.log(optGr.children.item(1).textContent, "this opt");
+//   Category.addEventListener("click", function () {
+//     // console.log("from contact us");
+//     // let sideBarForCategory = document.createElement("div");
+//     // sideBarForCategory.style.height = "20vh";
+//     // sideBarForCategory.style.width = 10%";
+//     // sideBarForCategory.style.backgroundColor = "white";
+//     // sideBarForCategory.style.opacity = "0.9";
+//     // sideBarForCategory.style.zIndex = "99999";
+//     // // Category.style.display = "flex";
+//     // sideBar.appendChild(sideBarForCategory);
+//   });
+
+//   // route
+//   const route = (event) => {
+//     event = event || window.event;
+//     event.preventDefault();
+//     window.history.pushState({}, "", event.target.href);
+//     handleLocation();
+//   };
+//   const routes = {
+//     404: "/404.html",
+//     "/": "/index.html",
+//     "/About": "/pages/About.html",
+//     "/ContactUs": "/pages/ContactUs.html",
+//   };
+//   const handleLocation = async () => {
+//     const path = window.location.pathname;
+//     const route = routes[path] || routes[404];
+//     await fetch(route).then((data) => data.text());
+//     // document.getElementById("main-page").innerHTML = html;
+//   };
+
+//   window.onpopstate = handleLocation;
+//   window.route = route;
+
+//   handleLocation();
+
+//   About.onclick = route();
+
+//   let allOptions = document.querySelectorAll("option");
+//   console.log(allOptions[0].value, "this is all for Option");
+//   // allOptions.forEach((e) => {
+//   //   console.log(e);
+//   function getSpecified() {
+//     if (Category.value !== "") {
+//       getData(
+//         `https://openlibrary.org/subjects/${Category.value}.json?limit=20`
+//       );
+//     }
+//   }
+
+//   // });
+//   //close Icon
+//   closeIcon.addEventListener("click", function () {
+//     sideBar.remove();
+//     menuIcon.style.display = "flex";
+//   });
+// }
